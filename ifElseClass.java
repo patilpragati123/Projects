fcc0f7f57e5ab65fae014ff307fdef6aa274f4e6
Pragati Rajaram Patil <125466723+patilpragati123@users.noreply.github.com>
2023-02-17 15:58:25 +0530

    Add files via upload

---
+++ ifElseClass.java
@@ -0,0 +1,16 @@
+public class ifElseClass {
+	public static void main(String[] args) {
+		//boolean true false
+		int x=10;
+		int y=11;
+		
+		
+		
+		if(x==y ) {
+			System.out.println("both are equal");
+		}else {
+			System.out.println("both are not equal");
+		}
+	}
+
+}
