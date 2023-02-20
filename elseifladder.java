f6ec4a1fe89406f625785a15a167bfdd8c64ee12
Pragati Rajaram Patil <125466723+patilpragati123@users.noreply.github.com>
2023-02-20 15:38:43 +0530

    Add files via upload

---
+++ elseifladder.java
@@ -0,0 +1,15 @@
+package com.tskjspider;
+
+public class elseifladder {
+	public static void main(String[] args) {
+		int a=7, b=9,c=6;
+		if (a>b&&a>c)
+			System.out.println(a);
+		else if (b>c) 
+         System.out.println(b);
+		else 
+			System.out.println(c);
+		
+	}
+
+}
