b1b9ab7be9f08bdcd68bb6239eab6a407be9ced3
Pragati Rajaram Patil <125466723+patilpragati123@users.noreply.github.com>
2023-02-20 15:38:59 +0530

    Add files via upload

---
+++ largenumber.java
@@ -0,0 +1,13 @@
+package com.tskjspider;
+
+public class largenumber {
+	public static void main(String[] args) {
+		int a=12;
+		int b=15;
+		if (a>b)
+			System.out.println("largest number"+a);
+		else 
+			System.out.println("largets number "+b);
+	}
+
+}
